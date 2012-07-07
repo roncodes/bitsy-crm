0bb8f2b0527d02c1dea6b6769f82bd6d4faa13d5
Ronald A. Richardson <theprestig3@gmail.com>
2012-07-07 13:49:50 -0700

    htaccess file and update

---
+++ application/logs/log-2012-07-07.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-07-07 20:47:38 --> Severity: Notice  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
+ C:\wamp\www\bitsy-crm\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2012-07-07 20:49:15 --> Severity: Notice  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
+ C:\wamp\www\bitsy-crm\system\database\drivers\mysql\mysql_driver.php 91
